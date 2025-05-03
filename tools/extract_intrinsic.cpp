016f702167e85db6adc999f88fa88caeff58066c
taewoo <taewoo.hong@patternlabs.ai>
2025-05-03 17:17:25 -0600

    fix BUILD file and add IR intrinsic extraction

--- tools/extract_intrinsic.cpp
+++ tools/extract_intrinsic.cpp
@@ -6,19 +6,40 @@
 #include <librealsense2/rs.hpp>
 #include <opencv2/core.hpp>
 
+
+void print_intrinsic_data(rs2_intrinsics data) {
+  std::cout << "============================================" << std::endl;
+  std::cout << "Width: " << data.width << std::endl;
+  std::cout << "Height: " << data.height << std::endl;
+  std::cout << "Focal length(fx, fy): " << data.fx << ", " << data.fy << std::endl;
+  std::cout << "Principal Point (cx, cy): " << data.ppx << ", " << data.ppy << std::endl;
+  std::cout << "Distortion Model: " << data.model << std::endl;
+  std::cout << "============================================" << std::endl << std::endl;
+return;
+}
+
 int main(int argc, char* argv[]){
   rs2::pipeline pipe;
-  pipe.start();
+  rs2::config cfg;
+
+  cfg.enable_stream(RS2_STREAM_COLOR, -1);
+  cfg.enable_stream(RS2_STREAM_INFRARED, 1);
+  pipe.start(cfg);
   rs2::frameset frames = pipe.wait_for_frames();
 
   rs2::video_frame color_frame = frames.get_color_frame();
-  rs2_intrinsics intrinsics = color_frame.get_profile().as<rs2::video_stream_profile>().get_intrinsics();
-  //rs2_extrinsics extrinsics = color_frame.get_profile().as<rs2::video_stream_profile>().get_extrinsics_to(const stream_profile &to)
+  rs2::video_frame ir_frame = frames.get_infrared_frame();
+  
+  rs2_intrinsics color_intrinsics = color_frame.get_profile().as<rs2::video_stream_profile>().get_intrinsics();
+  rs2_intrinsics ir_intrinsics = ir_frame.get_profile().as<rs2::video_stream_profile>().get_intrinsics();
 
-  std::cout << "Width: " << intrinsics.width << std::endl;
-  std::cout << "Height: " << intrinsics.height << std::endl;
-  std::cout << "Focal length(fx, fy): " << intrinsics.fx << ", " << intrinsics.fy << std::endl;
-  std::cout << "Principal Point (cx, cy): " << intrinsics.ppx << ", " << intrinsics.ppy << std::endl;
-  std::cout << "Distortion Model: " << intrinsics.model << std::endl;
+  //rs2_extrinsics extrinsics = color_frame.get_profile().as<rs2::video_stream_profile>().get_extrinsics_to(const stream_profile &to)
+  
+  std::cout << "COLOR STREAM" << std::endl;
+  print_intrinsic_data(color_intrinsics); 
+  
+  std::cout << "IR STREAM" << std::endl;
+  print_intrinsic_data(ir_intrinsics);
+
   return 0;
 }
