768f00b1b5b6b25d30c9867a42bb068fdbe896a2
taewoo <taewoo.hong@patternlabs.ai>
2025-05-04 11:27:53 -0600

    added extrinsics extraction from color to closest ir

--- tools/extract_intrinsics_and_extrinsics.cpp
+++ tools/extract_intrinsics_and_extrinsics.cpp
@@ -15,7 +15,30 @@ void print_intrinsic_data(rs2_intrinsics data) {
   std::cout << "Principal Point (cx, cy): " << data.ppx << ", " << data.ppy << std::endl;
   std::cout << "Distortion Model: " << data.model << std::endl;
   std::cout << "============================================" << std::endl << std::endl;
-return;
+  return;
+}
+
+void print_matrices(rs2_extrinsics data) {
+  std::cout << "============================================" << std::endl;
+  std::cout << "Extrinsics from color to IR: " << std::endl;
+  std::cout << "============================================" << std::endl;
+  std::cout << "Rotational matrix -" << std::endl;
+  for (int i = 0; i < 3; i++) {
+    std::cout << data.rotation[i] << " ";
+    if ((i + 1) % 3 == 0) {
+      std::cout << std::endl;
+    }
+  }
+  
+  std::cout << std::endl;
+  std::cout << "Translational matrix -" << std::endl;
+  for (int i = 0; i < 3; i++) {
+    std::cout << data.translation[i] << " ";
+    if ((i + 1) % 3 == 0) {
+       std::cout << std::endl;
+    }
+  }
+  return;
 }
 
 int main(int argc, char* argv[]){
@@ -30,16 +53,17 @@ int main(int argc, char* argv[]){
   rs2::video_frame color_frame = frames.get_color_frame();
   rs2::video_frame ir_frame = frames.get_infrared_frame();
   
-  rs2_intrinsics color_intrinsics = color_frame.get_profile().as<rs2::video_stream_profile>().get_intrinsics();
-  rs2_intrinsics ir_intrinsics = ir_frame.get_profile().as<rs2::video_stream_profile>().get_intrinsics();
-
-  //rs2_extrinsics extrinsics = color_frame.get_profile().as<rs2::video_stream_profile>().get_extrinsics_to(const stream_profile &to)
+  rs2::video_stream_profile color_profile = color_frame.get_profile().as<rs2::video_stream_profile>();
+  rs2::video_stream_profile ir_profile = ir_frame.get_profile().as<rs2::video_stream_profile>();
   
+  rs2_extrinsics color_to_ir_extrin = color_profile.get_extrinsics_to(ir_profile);
+
   std::cout << "COLOR STREAM" << std::endl;
-  print_intrinsic_data(color_intrinsics); 
+  print_intrinsic_data(color_profile.get_intrinsics()); 
   
   std::cout << "IR STREAM" << std::endl;
-  print_intrinsic_data(ir_intrinsics);
+  print_intrinsic_data(ir_profile.get_intrinsics());
 
+  print_matrices(color_to_ir_extrin);
   return 0;
 }
