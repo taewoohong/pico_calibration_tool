e53e2f9c0e27bc8fb7014dc0efeb42a3c004dc2d
taewoohong <taewoo.hong@patternlabs.ai>
2025-06-24 17:36:33 -0600

    added a script to run docker container

---
+++ docker/scripts/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run -it --rm \
+  --device=/dev/video0 \
+  --device=/dev/bus/usb/ \
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  --privileged \
+  --entrypoint /bin/bash test
